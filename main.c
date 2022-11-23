c768e3f8c2a96d6aa52a16e40ce43ac383015a40
Paweł Rybak <pawel.rybak13@gmail.com>
2022-11-23 22:35:38 +0100

    Fixed the byte size problem

--- main.c
+++ main.c
@@ -7,9 +7,10 @@
 #define WIDTH 1280
 #define HEIGHT 720
 
+//                  0xAABBGGRR
 #define COLOR_RED   0xFF0000FF
-#define COLOR_GREEN 0x00FF00FF
-#define COLOR_BLUE  0x0000FFFF
+#define COLOR_GREEN 0xFF00FF00
+#define COLOR_BLUE  0xFFFF0000
 
 static uint32_t image[HEIGHT][WIDTH];
 
@@ -37,10 +38,10 @@ void create_ppm_image(const char *file_path) {
     for (size_t y = 0; y < HEIGHT; y++) {
         for (size_t x = 0; x < WIDTH; x++) {
             uint32_t pixel = image[y][x];
-            uint32_t rgb_bytes_arr[3] = {
-                (pixel & 0xFF0000) >> 8*0,
+            uint8_t rgb_bytes_arr[3] = {
+                (pixel & 0x0000FF) >> 8*0,
                 (pixel & 0x00FF00) >> 8*1,
-                (pixel & 0x0000FF) >> 8*2
+                (pixel & 0xFF0000) >> 8*2
             };
             fwrite(rgb_bytes_arr, sizeof(rgb_bytes_arr), 1, file);
         }
