ca946fe845aa02e83cb8b271928ebd3bc6075fc0
Paweł Rybak <pawel.rybak13@gmail.com>
2022-11-26 21:31:46 +0100

    Random node generation

--- main.c
+++ main.c
@@ -3,21 +3,33 @@
 #include <stdint.h>
 #include <errno.h>
 #include <string.h>
+#include <time.h>
 
 #define WIDTH 600
 #define HEIGHT 400
+
+#define NODE_COUNT 15
+#define NODE_RADIUS 3
 
 //                  0xAABBGGRR
 #define COLOR_RED   0xFF0000FF
 #define COLOR_GREEN 0xFF00FF00
 #define COLOR_BLUE  0xFFFF0000
 
-static uint32_t image[HEIGHT][WIDTH];
-
 typedef struct {
     int x;
     int y;
 } Node;
+
+static uint32_t image[HEIGHT][WIDTH];
+static Node nodes[NODE_COUNT];
+
+void generate_nodes() {
+    for (size_t i = 0; i < NODE_COUNT; i++) {
+        nodes[i].x = rand() % WIDTH;
+        nodes[i].y = rand() % HEIGHT;
+    }
+}
 
 void fill_image(uint32_t color) {
     for (size_t y = 0; y < HEIGHT; y++) {
@@ -77,13 +89,16 @@ void create_ppm_image(const char *file_path) {
 }
 
 int main() {
-    Node test_node;
-    test_node.x = 128;
-    test_node.y = 128;
-
     fill_image(COLOR_BLUE);
-    fill_circle(test_node, 10, COLOR_RED);
+
+    srand((unsigned)time(NULL));
+    generate_nodes();
+
+    for (size_t i = 0; i < NODE_COUNT; i++) {
+        fill_circle(nodes[i], NODE_RADIUS, COLOR_RED);
+    }
 
     create_ppm_image("generated-voronoi.ppm");
+
     return 0;
 }
